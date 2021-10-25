ec03d2291be592c178b2ead16392de4275f6c5e5
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-10-25 15:08:45 +0530

    files added.

--- app/src/main/java/com/example/event_listeners_assignment/MainActivity.java
+++ app/src/main/java/com/example/event_listeners_assignment/MainActivity.java
@@ -36,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
         mEt_box.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
 
             @Override
